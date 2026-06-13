7c34e69d5edee025ab90c2dd17e2d84fa5d6075c
Subodh Kumar <subodh.kumar19822026@gmail.com>
2026-06-13 10:21:47 +0530

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SubodhProjet3030.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
